bd872865a7f63c3921c65dc7355ff8802963ca9d
Peter Hazell <18005888+peter-hazell@users.noreply.github.com>
2023-04-03 13:23:28 +0100

    Use LocalDate for dateOfIncorporation (#41)

--- app/uk/gov/hmrc/economiccrimelevyregistration/models/grs/IncorporatedEntityJourneyData.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/models/grs/IncorporatedEntityJourneyData.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.economiccrimelevyregistration.models.grs
 
 import play.api.libs.json.{Json, OFormat}
 
-import java.util.Date
+import java.time.LocalDate
 
 final case class IncorporatedEntityJourneyData(
   companyProfile: CompanyProfile,
@@ -36,13 +36,12 @@ object IncorporatedEntityJourneyData {
 final case class CompanyProfile(
   companyName: String,
   companyNumber: String,
-  dateOfIncorporation: Date,
+  dateOfIncorporation: LocalDate,
   unsanitisedCHROAddress: IncorporatedEntityAddress
 )
 
 object CompanyProfile {
-  implicit val format: OFormat[CompanyProfile] =
-    Json.format[CompanyProfile]
+  implicit val format: OFormat[CompanyProfile] = Json.format[CompanyProfile]
 }
 
 final case class IncorporatedEntityAddress(
