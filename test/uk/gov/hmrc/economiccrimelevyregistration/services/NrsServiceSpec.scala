4eb861cbb68bb260cb12dbcdab86ba9ab5311299
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-04-05 16:42:53 +0100

    Added NRS service tests

--- test/uk/gov/hmrc/economiccrimelevyregistration/services/NrsServiceSpec.scala
+++ test/uk/gov/hmrc/economiccrimelevyregistration/services/NrsServiceSpec.scala
@@ -16,23 +16,82 @@
 
 package uk.gov.hmrc.economiccrimelevyregistration.services
 
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.any
 import uk.gov.hmrc.economiccrimelevyregistration.base.SpecBase
 import uk.gov.hmrc.economiccrimelevyregistration.connectors.NrsConnector
+import uk.gov.hmrc.economiccrimelevyregistration.generators.CachedArbitraries._
+import uk.gov.hmrc.economiccrimelevyregistration.models.nrs._
+import uk.gov.hmrc.economiccrimelevyregistration.models.requests.AuthorisedRequest
+import uk.gov.hmrc.http.UpstreamErrorResponse
 
-import java.time.temporal.ChronoUnit
 import java.time.{Clock, Instant, ZoneId}
+import scala.concurrent.Future
 
 class NrsServiceSpec extends SpecBase {
 
   val mockNrsConnector: NrsConnector = mock[NrsConnector]
-  val now: Instant                   = Instant.now.truncatedTo(ChronoUnit.MILLIS)
-  private val clock: Clock           = Clock.fixed(now, ZoneId.systemDefault)
+  private val fixedPointInTime       = Instant.parse("2007-12-25T10:15:30.00Z")
+  private val stubClock: Clock       = Clock.fixed(fixedPointInTime, ZoneId.systemDefault)
 
-  val service = new NrsService(mockNrsConnector, clock)
+  val service = new NrsService(mockNrsConnector, stubClock)
 
   "submitToNrs" should {
-    "" in {
-      pending
+    "return the NRS submission ID when the submission is successful" in forAll {
+      (
+        nrsSubmission: NrsSubmission,
+        nrsSubmissionResponse: NrsSubmissionResponse,
+        base64EncodedNrsSubmissionHtml: String,
+        eclRegistrationReference: String,
+        businessPartnerId: String,
+        request: AuthorisedRequest[_]
+      ) =>
+        when(mockNrsConnector.submitToNrs(ArgumentMatchers.eq(nrsSubmission))(any()))
+          .thenReturn(Future.successful(nrsSubmissionResponse))
+
+        val result =
+          await(
+            service.submitToNrs(Some(base64EncodedNrsSubmissionHtml), eclRegistrationReference, businessPartnerId)(
+              hc,
+              request
+            )
+          )
+
+        result shouldBe nrsSubmissionResponse
+    }
+
+    "throw an IllegalStateException when there is no base64 encoded NRS submission HTML" in forAll {
+      (eclRegistrationReference: String, businessPartnerId: String, request: AuthorisedRequest[_]) =>
+        val result = intercept[IllegalStateException] {
+          await(service.submitToNrs(None, eclRegistrationReference, businessPartnerId)(hc, request))
+        }
+
+        result.getMessage shouldBe "Base64 encoded NRS submission HTML not found in registration data"
+    }
+
+    "throw an exception when the submission fails" in forAll {
+      (
+        nrsSubmission: NrsSubmission,
+        base64EncodedNrsSubmissionHtml: String,
+        eclRegistrationReference: String,
+        businessPartnerId: String,
+        request: AuthorisedRequest[_]
+      ) =>
+        val error = UpstreamErrorResponse("Internal server error", INTERNAL_SERVER_ERROR)
+
+        when(mockNrsConnector.submitToNrs(ArgumentMatchers.eq(nrsSubmission))(any()))
+          .thenReturn(Future.failed(error))
+
+        val result = intercept[UpstreamErrorResponse] {
+          await(
+            service.submitToNrs(Some(base64EncodedNrsSubmissionHtml), eclRegistrationReference, businessPartnerId)(
+              hc,
+              request
+            )
+          )
+        }
+
+        result shouldBe error
     }
   }
 
