f76e72a5a57074e06944ea94cc79b3bce422f4a8
Tony Ogilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-05-16 12:29:55 +0100

    ECL-394: Added Unlimited Company entity type (#54)

--- app/uk/gov/hmrc/economiccrimelevyregistration/models/EntityType.scala
+++ app/uk/gov/hmrc/economiccrimelevyregistration/models/EntityType.scala
@@ -28,6 +28,7 @@ object EntityType {
   case object LimitedPartnership extends EntityType
   case object ScottishLimitedPartnership extends EntityType
   case object LimitedLiabilityPartnership extends EntityType
+  case object UnlimitedCompany extends EntityType
 
   case object Other extends EntityType
 
@@ -42,6 +43,7 @@ object EntityType {
           case "LimitedPartnership"          => JsSuccess(LimitedPartnership)
           case "ScottishLimitedPartnership"  => JsSuccess(ScottishLimitedPartnership)
           case "LimitedLiabilityPartnership" => JsSuccess(LimitedLiabilityPartnership)
+          case "UnlimitedCompany"            => JsSuccess(UnlimitedCompany)
           case "Other"                       => JsSuccess(Other)
           case s                             => JsError(s"$s is not a valid EntityType")
         }
