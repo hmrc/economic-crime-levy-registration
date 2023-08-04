1cff80ddf7d0c04aece8440af55b0c59677fa2ca
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2023-08-04 09:22:19 +0100

    ECL-335: 2023-08-04

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.19.0"
+  private val hmrcBootstrapVersion = "7.21.0"
   private val hmrcMongoVersion     = "1.3.0"
   private val openHtmlToPdfVersion = "1.0.10"
 
@@ -13,14 +13,14 @@ object AppDependencies {
     "org.typelevel"     %% "cats-core"                         % "2.9.0",
     "io.circe"          %% "circe-json-schema"                 % "0.2.0",
     "io.circe"          %% "circe-parser"                      % "0.14.5",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-28"      % "1.2.0",
-    "com.beachape"      %% "enumeratum-play-json"              % "1.7.2"
+    "uk.gov.hmrc"       %% "internal-auth-client-play-28"      % "1.6.0",
+    "com.beachape"      %% "enumeratum-play-json"              % "1.7.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
-    "org.mockito"          %% "mockito-scala"            % "1.17.12",
+    "org.mockito"          %% "mockito-scala"            % "1.17.14",
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
     "io.circe"             %% "circe-json-schema"        % "0.2.0",
